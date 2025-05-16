6b229b92dd3501b6ba03888d7df37d2f3c4723ac
Himanshuu <2203himanshusingh@gmail.com>
2025-05-17 04:36:17 +0530

    qwe

--- front-end/easy-Life-Care/src/components/menuComp.jsx
+++ front-end/easy-Life-Care/src/components/menuComp.jsx
@@ -1,9 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ContactUs from "../pages/contactUs";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
